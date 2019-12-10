7a8d56d204f2a738ec3bedecb3c2dd85c174ab32
afrayz29 <alfrayman@gmail.com>
2019-12-10 15:25:59 +0000

    update

--- src/js/index.js
+++ src/js/index.js
@@ -7,11 +7,10 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-    let suitArray = [];
-  let numberArray =[""]
-
-  let suit = suitArray[Math.floor(Math.random()=suiteArray.length)];
-  let numb = numberArray[Math.floor(Math.random())=numberArray.length)];
+    let suitArray = ["&#9824", "&#9827", "&#9829", "&#9830"];
+  let numberArray =["A",2,3,4,5,6,7,8,9,10,"J","Q","K"];
+  let suit = suitArray[Math.floor(Math.random()*suiteArray.length)];
+  let numb = numberArray[Math.floor(Math.random()*numberArray.length)];
   console.log("suit",suit);
   console.log("numb",numb);
   if (suit==="") || suit ===""){
